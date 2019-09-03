83b4d367700ca021ab4799ee86fd1583409c548b
Albert Viilik <albert.viilik@gmail.com>
2019-09-03 14:33:36 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,6 +31,7 @@ end
 def move(board, index, player = "X")
   if valid_move?(board, index)
     board[index] = player
+    display_board(board)
   end
 end
 
@@ -40,7 +41,6 @@ def turn(board)
   input = input_to_index(input)
   if valid_move?(board, input)
     move(board, input)
-    display_board(board)
   end
   until true
     puts "Please enter 1-9:"
