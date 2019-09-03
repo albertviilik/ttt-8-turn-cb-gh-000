0317c4a5ee610f46403ad49eecc614c67ce137e1
Albert Viilik <albert.viilik@gmail.com>
2019-09-03 14:29:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,12 +40,11 @@ def turn(board)
   if valid_move?(board, input)
     move(board, input)
     display_board(board)
-  else
-    until valid_move?(board, input)
-      puts "Please enter 1-9:"
-      input = input_to_index(gets.to_i)
-    end
-    move(board, input)
-    display_board(board)
   end
+  until valid_move?(board, input)
+    puts "Please enter 1-9:"
+    input = input_to_index(gets.to_i)
+  end
+  move(board, input)
+  display_board(board)
 end
