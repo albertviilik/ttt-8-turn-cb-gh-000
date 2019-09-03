cbbf4e5faff762f244fd4caeb84acd20f586cac0
Albert Viilik <albert.viilik@gmail.com>
2019-09-03 15:27:35 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,7 +37,7 @@ def turn(board)
   puts "Please enter 1-9:"
   index = input_to_index(gets.to_i)
   if valid_move?(board, index)
-
+    move(board, index)
   else
     index = input_to_index(gets.to_i)
   end
